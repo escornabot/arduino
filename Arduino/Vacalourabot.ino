f67eb2e14c107ebeb5f29bb810d08e258105fbd3
Rafa Couto <caligari@treboada.net>
2014-09-05 22:30:41 +0200

    Refactorización do programa principal.

--- Arduino/Vacalourabot.ino
+++ Arduino/Vacalourabot.ino
@@ -3,102 +3,61 @@
 #include "Vacalourabot.h"
 
 
-/*
-void irPdvNeno()
+void go()
 {
-    // supoñer que se está detrás da vacalourabot
-    byte direccion_anterior = MOVE_FORWARD;
-
-    // por cada un dos movementos
-    for (int m = 0; m < PROGRAM->move_count; m++)
-    {
-        // calcular o xiro dende o punto de vista actual
-        int xiro = PROGRAM->move_list[m] - direccion_anterior;
-
-        // axustar xiro para o lado máis curto
-        if (abs(xiro) == 3) xiro /= -xiro;
-
-        // executar o xiro
-        xirar90(xiro);
-
-        // avanzar un paso cara adiante
-        avanzar(1, SENTIDO_ADIANTE);
-
-        // actualizar dirección actual
-        direccion_anterior = PROGRAM->move_list[m];
-    }
-}
-*/
-
-void irPdvVacaloura()
-{
-    // por cada un dos movementos
-    for (int m = 0; m < PROGRAM->getMoveCount(); m++)
-    {
-        // evalúa o tipo e procede
-        switch (PROGRAM->getMove(m))
-        {
-            case MOVE_RIGHT:
-                ENGINE->turn90Degrees(1);
-                break;
-            case MOVE_LEFT:
-                ENGINE->turn90Degrees(-1);
-                break;
-            case MOVE_FORWARD:
-                ENGINE->moveStraight(1);
-                break;
-            case MOVE_BACKWARD:
-                ENGINE->moveStraight(-1);
-                break;
-        }
-    }
-}
-
-void ir()
-{
-    // só imos se hai movementos programados
+    // only with movements
     if (PROGRAM->getMoveCount() > 0)
     {
         #if USE_PERSISTENT_MEMORY
 
-        // gardamos o programa de movementos na EEPROM
+        // save the movement program in EEPROM
         PROGRAM->save();
 
         #endif
 
-        // pequena pausa para dar tempo a soltar o botón
-        delay(1000);
+        // let user to release the 'go' button before the action
+        delay(500);
 
-        // evaluar o punto de vista para execución de movementos
-        if (PROGRAM->getPointOfView() == POV_VACALOURA)
+        // move by move
+        for (int m = 0; m < PROGRAM->getMoveCount(); m++)
         {
-            // punto de vista da vacaloura
-            irPdvVacaloura();
-        }
-        else
-        {
-            // punto de vista do neno
-//      irPdvNeno();
+            // which move
+            switch (PROGRAM->getMove(m))
+            {
+                case MOVE_RIGHT:
+                    ENGINE->turn90Degrees(1);
+                    break;
+                case MOVE_LEFT:
+                    ENGINE->turn90Degrees(-1);
+                    break;
+                case MOVE_FORWARD:
+                    ENGINE->moveStraight(1);
+                    break;
+                case MOVE_BACKWARD:
+                    ENGINE->moveStraight(-1);
+                    break;
+            }
         }
     }
 }
 
 
-void memorizar(MOVE movemento)
+void storeMove(MOVE move)
 {
     if (PROGRAM->getMoveCount() < MOVE_LIMIT)
     {
-        PROGRAM->addMove(movemento);
+        PROGRAM->addMove(move);
     }
     else
     {
-        ir();
+        // memory is full
+        go();
     }
 }
 
 void setup()
 {
-    // init hardware
+    // init engine
     ENGINE->init();
 
     // init button set
@@ -106,7 +65,7 @@ void setup()
 
     #if USE_PERSISTENT_MEMORY
 
-    // cargar os movementos logo dun apagado
+    // restore last program
     PROGRAM->load();
 
     #endif
@@ -119,23 +78,23 @@ void loop(){
     switch (BUTTONS->scanButtons()) {
 
         case ButtonSet::BUTTON_UP:
-            memorizar(MOVE_FORWARD);
+            storeMove(MOVE_FORWARD);
             break;
 
         case ButtonSet::BUTTON_RIGHT:
-            memorizar(MOVE_RIGHT);
+            storeMove(MOVE_RIGHT);
             break;
 
         case ButtonSet::BUTTON_DOWN:
-            memorizar(MOVE_BACKWARD);
+            storeMove(MOVE_BACKWARD);
             break;
 
         case ButtonSet::BUTTON_LEFT:
-            memorizar(MOVE_LEFT);
+            storeMove(MOVE_LEFT);
             break;
 
         case ButtonSet::BUTTON_GO:
-            ir();
+            go();
             break;
 
         case ButtonSet::BUTTON_RESET:
