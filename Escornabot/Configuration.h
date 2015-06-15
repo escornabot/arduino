5ad7b975ba19229c359fad3f24acc6ab4abc6767
Xoan Sampaiño <xoansampainho@gmail.com>
2015-06-15 19:15:00 +0200

    Fixed Brivoi step lentgh for STEPPERS_LINE_STEPS.

--- Escornabot/Configuration.h
+++ Escornabot/Configuration.h
@@ -94,7 +94,7 @@ See LICENSE.txt for details
 
 // step calibration
 #define STEPPERS_STEPS_PER_SECOND 1000
-#define STEPPERS_LINE_STEPS 1500
+#define STEPPERS_LINE_STEPS 1738
 #define STEPPERS_TURN_STEPS 1024
 
 #endif
